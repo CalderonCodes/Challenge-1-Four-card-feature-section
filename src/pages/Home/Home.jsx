23874c0c63adc574a7664568c522dfdd5d0d3bd9
CalderonCodes <cgcalderondev@gmail.com>
2023-10-30 01:29:00 -0600

    :sparkles: Add home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,22 @@
+
+
+function Home() {
+  return (
+    <div className='flex flex-col items-center bg-VeryLightGray min-h-screen lg:p-12 py-20'>
+        <h1 className="text-GrayishBlue font-Poppins font-extralight text-3xl my-1">Reliable, efficient delivery </h1>
+        <h1 className="text-VeryDarkBlue font-Poppins text-3xl font-extrabold my-1">Powered by Technology</h1>
+        <p className="text-GrayishBlue font-Poppins font-normal text-center lg:text-sm text-lg lg:w-1/3 px-16 lg:px-0 my-3">
+        Our Artificial Intelligence powered tools use millions of project data points 
+  to ensure that your project is successful
+        </p>
+
+        <div className="flex flex-col lg:flex-row w-full h-full gap-10 lg:justify-center items-center mt-10">
+          
+          
+        </div>
+       
+    </div>
+  )
+}
+
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", "./index.html",
+  ],
   theme: {
+    colors: {
+      Red: 'hsl(0, 78%, 62%)',
+      Cyan: 'hsl(180, 62%, 55%)',
+      Orange: 'hsl(34, 97%, 64%)',
+      Blue: 'hsl(212, 86%, 64%)',
+      VeryDarkBlue: 'hsl(234, 12%, 34%)',
+      GrayishBlue: 'hsl(229, 6%, 66%)',
+      VeryLightGray: 'hsl(0, 0%, 98%)',
+    },
     extend: {},
+    fontFamily: {
+      Poppins: ['Poppins']
+    }
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
 
+  daisyui: {
+    themes: ["light"],
+  },
+}
