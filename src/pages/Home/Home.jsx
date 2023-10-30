9102a59db7cfafde39c6a921cf7b009dfa8bc635
CalderonCodes <cgcalderondev@gmail.com>
2023-10-30 01:52:49 -0600

    :sparkles: Add responsiveness to home structure

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,15 +2,15 @@ import { Card } from "../../components/Card/Card"
 
 function Home() {
   return (
-    <div className='flex flex-col items-center bg-VeryLightGray max-w-screen h-screen p-12'>
+    <div className='flex flex-col items-center bg-VeryLightGray max-w-screen min-h-screen lg:p-12 py-20'>
         <h1 className="text-GrayishBlue font-Poppins font-extralight text-3xl my-1">Reliable, efficient delivery </h1>
         <h1 className="text-VeryDarkBlue font-Poppins text-3xl font-extrabold my-1">Powered by Technology</h1>
-        <p className="text-GrayishBlue font-Poppins font-normal text-center text-sm w-1/3 my-3">
+        <p className="text-GrayishBlue font-Poppins font-normal text-center lg:text-sm text-lg lg:w-1/3 px-16 lg:px-0 my-3">
         Our Artificial Intelligence powered tools use millions of project data points 
   to ensure that your project is successful
         </p>
 
-        <div className="flex w-full h-full gap-10 justify-center mt-10">
+        <div className="flex flex-col lg:flex-row w-full h-full gap-10 lg:justify-center items-center mt-10">
           <div className="flex flex-col h-full justify-center">
             <Card title="Supervisor" content="Monitors activity to identify project roadblocks" 
             border="border-Cyan" image="/src/assets/icon-supervisor.svg"/>
