71429acbb887ecce9b56bc3f6df42b090abf78b7
CalderonCodes <cgcalderondev@gmail.com>
2023-10-30 17:36:14 -0600

    :lipstick: Add frontend mentor footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import { Card } from "../../components/Card/Card"
 
 function Home() {
   return (
-    <div className='flex flex-col items-center bg-VeryLightGray min-h-screen lg:p-12 py-20'>
+    <div className='flex flex-col items-center bg-VeryLightGray min-h-screen  max-h-screen lg:p-12 py-20'>
         <h1 className="text-GrayishBlue font-Poppins font-extralight text-3xl my-1">Reliable, efficient delivery </h1>
         <h1 className="text-VeryDarkBlue font-Poppins text-3xl font-extrabold my-1">Powered by Technology</h1>
         <p className="text-GrayishBlue font-Poppins font-normal text-center lg:text-sm text-lg lg:w-1/3 px-16 lg:px-0 my-3">
@@ -14,23 +14,30 @@ function Home() {
           
           <div className="flex flex-col h-full w-full lg:w-1/5 justify-center items-center">
             <Card title="Supervisor" content="Monitors activity to identify project roadblocks" 
-            border="border-Cyan" image="src/assets/icon-supervisor.svg"/>
+            border="border-Cyan" image="assets/icon-supervisor.svg"/>
           </div>
 
           <div className="flex flex-col h-full w-full lg:w-1/5 justify-center items-center gap-7">
             <Card title="Team Builder" content="Scans our talent network to create the optimal team for your project" 
-            border="border-Red" image="src/assets/icon-team-builder.svg"/>
+            border="border-Red" image="assets/icon-team-builder.svg"/>
             <Card title="Karma" content="Regularly evaluates our talent to ensure quality" 
-            border="border-Orange" image="src/assets/icon-karma.svg"/>
+            border="border-Orange" image="assets/icon-karma.svg"/>
           </div>
 
           <div className="flex flex-col h-full w-full lg:w-1/5 justify-center items-center">
             <Card title="Calculator" content="Uses data from past projects to provide better delivery estimates" 
-            border="border-Blue" image="src/assets/icon-calculator.svg"/>
+            border="border-Blue" image="assets/icon-calculator.svg"/>
           </div>
 
         </div>
        
+        <div className="flex flex-col montserrat-500 text-center text-sm text-GrayishBlue mt-5">
+                <div>
+                    Challenge by <a className="underline text-super-dark-cyan" href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+                </div>
+                <div>Coded by <a className="underline italic" href="https://github.com/CalderonCodes">Christopher Calderon</a>.</div>
+            </div>
+
     </div>
   )
 }
