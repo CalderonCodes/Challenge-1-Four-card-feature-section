c91c1e7f51165b420deaa70bbfb2f0a9391c1372
CalderonCodes <cgcalderondev@gmail.com>
2023-10-30 02:08:25 -0600

    :sparkles: Add responsiveness to card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,10 +2,10 @@
 export const Card = ({ title = "Default", content = "0000000000", border = "border-green-500", image = "default.jpg"}) => {
   const borderClass = `${border}`;
 return (
-  <div className={`card lg:w-72 w-96 lg:h-52 h-64 bg-base-100 rounded-md border-t-4 ${borderClass} shadow-2xl shadow-GrayishBlue p-0 flex`}>
+  <div className={`card lg:w-72 w-10/12 lg:h-52 h-64 bg-base-100 rounded-md border-t-4  ${borderClass} shadow-2xl shadow-GrayishBlue p-0 flex`}>
    <div className="card-body p-7">
        <h2 className="card-title text-VeryDarkBlue font-Poppins font-bold">{title}</h2>
-       <p className='text-GrayishBlue font-Poppins font-normal text-xs'>{content}</p>
+       <p className='text-GrayishBlue font-Poppins font-normal lg:text-xs text-base'>{content}</p>
        <div className="card-actions justify-end">
        <img src={image} alt="playlist" />
   </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import { Card } from "../../components/Card/Card"
 
 function Home() {
   return (
-    <div className='flex flex-col items-center bg-VeryLightGray max-w-screen min-h-screen lg:p-12 py-20'>
+    <div className='flex flex-col items-center bg-VeryLightGray min-h-screen lg:p-12 py-20'>
         <h1 className="text-GrayishBlue font-Poppins font-extralight text-3xl my-1">Reliable, efficient delivery </h1>
         <h1 className="text-VeryDarkBlue font-Poppins text-3xl font-extrabold my-1">Powered by Technology</h1>
         <p className="text-GrayishBlue font-Poppins font-normal text-center lg:text-sm text-lg lg:w-1/3 px-16 lg:px-0 my-3">
@@ -11,18 +11,20 @@ function Home() {
         </p>
 
         <div className="flex flex-col lg:flex-row w-full h-full gap-10 lg:justify-center items-center mt-10">
-          <div className="flex flex-col h-full justify-center">
+          
+          <div className="flex flex-col h-full w-full lg:w-1/5 justify-center items-center">
             <Card title="Supervisor" content="Monitors activity to identify project roadblocks" 
             border="border-Cyan" image="/src/assets/icon-supervisor.svg"/>
           </div>
-          <div className="flex flex-col h-full justify-center gap-7">
+
+          <div className="flex flex-col h-full w-full lg:w-1/5 justify-center items-center gap-7">
             <Card title="Team Builder" content="Scans our talent network to create the optimal team for your project" 
             border="border-Red" image="/src/assets/icon-team-builder.svg"/>
             <Card title="Karma" content="Regularly evaluates our talent to ensure quality" 
             border="border-Orange" image="/src/assets/icon-karma.svg"/>
           </div>
-          <div className="flex flex-col h-full justify-center">
-          
+
+          <div className="flex flex-col h-full w-full lg:w-1/5 justify-center items-center">
             <Card title="Calculator" content="Uses data from past projects to provide better delivery estimates" 
             border="border-Blue" image="/src/assets/icon-calculator.svg"/>
           </div>
